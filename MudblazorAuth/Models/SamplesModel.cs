8a43f282259c704daf604ad7a421661437e91758
vernorS <106086407+vernorS@users.noreply.github.com>
2022-07-20 13:32:25 +0100

    Initial commit

---
+++ MudblazorAuth/Models/SamplesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MudblazorAuth.Models
+{
+    public class SamplesModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? Zip { get; set; }
+        public string? Txt { get; set; }
+    }
+}
